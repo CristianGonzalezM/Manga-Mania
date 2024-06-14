4680e60e4c5b7099c01e7ca6829ddc79c62202dd
Default Profile <dix.campos@duocuc.cl>
2024-06-14 04:25:30 -0400

    .

--- BackEnd/BiblioMania/models.py
+++ BackEnd/BiblioMania/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Manga(models.Model):
     STATUS_CHOICES = [
         ('E', 'En emisión'),
@@ -17,11 +16,11 @@ class Manga(models.Model):
         return f'{self.title} by {self.author} - {self.status}'
 
 class Resena(models.Model):
-    manga = models.ForeignKey(Manga, on_delete=models.CASCADE)
+    manga = models.ForeignKey(Manga, on_delete=models.CASCADE, related_name='resenas')
     user = models.CharField(max_length=100)
     rating = models.IntegerField()
     comment = models.TextField()
     review_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return f'Reseña de {self.user} del Manga {self.manga.title} con una puntuacion de {self.rating}'
+        return f'Reseña de {self.user} del Manga {self.manga.title} con una puntuación de {self.rating}'
